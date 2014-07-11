8c80b20299d998c358ffe84be1aa48138f1534f0
杨博 <pop.atry@gmail.com>
2014-07-11 14:58:13 +0800

    Predef应该是extern class

--- src/haxe/scala/Predef.hx
+++ src/haxe/scala/Predef.hx
@@ -1,10 +1,10 @@
 package scala;
 
-class Predef
+extern class Predef
 {
 }
 
 @:native("scala.Predef$$less$colon$less")
-class Predef_24_24less_24colon_24less<A, B>
+extern class Predef_24_24less_24colon_24less<A, B>
 {
 }
