da80fabbc7ca1a6a7f8deb9c21ad7256d5141368
杨博 <pop.atry@gmail.com>
2014-07-12 23:28:42 +0800

    增加@:overload关键字

--- src/haxe/scala/Function1.hx
+++ src/haxe/scala/Function1.hx
@@ -2,6 +2,6 @@ package scala;
 
 @:realPath("scala.Function1") @:abstract @:javaNative @:native("scala.Function1") extern interface Function1<T1,R> {
 	@:overload function andThen<A>(param1 : Function1<R,A>) : Function1<T1,A>;
-	@:overload function apply(param1 : T1) : R;
+	function apply(param1 : T1) : R;
 	@:overload function compose<A>(param1 : Function1<A,T1>) : Function1<A,R>;
 }
