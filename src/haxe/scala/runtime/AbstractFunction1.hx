f455356afeeb8892ba581e75117ff4a38daded01
杨博 (Yang Bo) <pop.atry@gmail.com>
2014-10-31 01:59:52 +0800

    Update AbstractFunction1.hx

--- src/haxe/scala/runtime/AbstractFunction1.hx
+++ src/haxe/scala/runtime/AbstractFunction1.hx
@@ -19,9 +19,10 @@
 
 package scala.runtime;
 
-@:abstract extern class AbstractFunction1<T1,R> extends java.lang.Object {
-	@:overload function new();
-	@:overload function andThen<A>(param1 : Function1<R,A>) : Function1<T1,A>;
+@:abstract extern class AbstractFunction1<T1,R> {
+	function new();
+	function andThen<A>(param1 : Function1<R,A>) : Function1<T1,A>;
 	function apply(param1 : T1) : R;
-	@:overload function compose<A>(param1 : Function1<A,T1>) : Function1<A,R>;
+	function compose<A>(param1 : Function1<A,T1>) : Function1<A,R>;
+	function toString():String;
 }
